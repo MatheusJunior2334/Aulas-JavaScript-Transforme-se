47db046a5a12ab455b68fda1a252a6503689d12f
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2026-03-18 22:23:00 -0300

    Aula 15: Estudando Manipulações de Elementos no HTML

---
+++ Aula15/src/script03.js
@@ -0,0 +1,7 @@
+// Utilizando o innerHTML
+document.querySelector(".mudandoElemento").innerHTML = "Eleanor Marques";
+
+// Alterando atributos
+const elemento = document.querySelector(".foto");
+
+elemento.src = "./Spirited_Away.webp"
